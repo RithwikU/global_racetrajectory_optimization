01b0cf0d2009f5599a9b8dee70678e4c851e8e0b
Rithwik Udayagiri <rithwik.math@gmail.com>
2023-04-19 13:35:55 -0400

    New center line | tighter params

--- main_globaltraj.py
+++ main_globaltraj.py
@@ -42,7 +42,7 @@ plot_opts = {"mincurv_curv_lin": False,         # plot curv. linearization (orig
 # file_paths["track_name"] = "handling_track"                                 # artificial track
 # file_paths["track_name"] = "berlin_2018"                                    # Berlin Formula E 2018
 # file_paths["track_name"] = "modena_2019"                                    # Modena 2019
-file_paths["track_name"] = "waypoints_finalrace_1"
+file_paths["track_name"] = "waypoints_skirk2"
 
 # set import options ---------------------------------------------------------------------------------------------------
 imp_opts = {"flip_imp_track": False,                # flip imported track to reverse direction
